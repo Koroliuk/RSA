7142de92547dfabeb1f5ff1e5cf4ddc9466a3a3d
Koroliuk <54640416+Koroliuk@users.noreply.github.com>
2020-05-23 21:41:39 +0300

    Add files via upload

---
+++ files/Key.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Key {
+    public long firstNum;
+    public long secondNum;
+
+    Key(long firstNum, long secondNum) {
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+    }
+}
