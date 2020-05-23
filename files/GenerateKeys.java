75a70f03898f753f3e62a447f86958b35b0bb3ad
Koroliuk <54640416+Koroliuk@users.noreply.github.com>
2020-05-23 21:41:59 +0300

    Add files via upload

---
+++ files/GenerateKeys.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class GenerateKeys {
+
+    public static Key[] generateKeys(long prime1, long prime2) {
+        long module = prime1*prime2;
+        long phi = (prime1-1)*(prime2-1);
+        long publicExponent = 65537; // One of Fermat prime numbers
+        long secretExponent = inverseModule(publicExponent, phi);
+        Key[] pairOfKeys = new Key[2];
+        pairOfKeys[0] = new Key(publicExponent, module);
+        pairOfKeys[1] = new Key(secretExponent, module);
+        return pairOfKeys;
+    }
+
+    private static long inverseModule(long num, long module) {
+        long[] result = gcdExtended(num, module);
+        long x = Math.abs(result[1]);
+        return (x % module + module) % module;
+    }
+
+    private static long[] gcdExtended(long a, long b) {
+        if (a == 0L) {
+            return new long[]{b, 0L, 1L};
+        }
+        long[] result = gcdExtended(b%a, a);
+        long d = result[0];
+        long x1 = result[1];
+        long y1 = result[2];
+        long x = y1-(b/a)*x1;
+        long y = x1;
+        return new long[]{d, x, y};
+    }
+}
