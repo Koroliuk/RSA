8862db2b491cd391f55af274f051df8e8fe4d620
Koroliuk <54640416+Koroliuk@users.noreply.github.com>
2020-06-05 02:30:06 +0300

    Побудувано проект через Maven
    Побудувано проект через Maven, як наслідок додано файл залежностей pom.xml

---
+++ src/main/java/company/Key.java
@@ -0,0 +1,21 @@
+package company;
+
+
+public class Key {
+
+    private final long firstNum;
+    private final long secondNum;
+
+    public Key(final long firstNum, final long secondNum) {
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+    }
+
+    public long getFirstNum() {
+        return firstNum;
+    }
+
+    public long getSecondNum() {
+        return secondNum;
+    }
+}
